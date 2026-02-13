82a12ad82d79fa1afae0a88ae6a1e81b5edcad20
IanHewlett <ian.w.hewlett@gmail.com>
2026-02-13 00:28:05 -0800

    add pokedex command

--- src/commands.ts
+++ src/commands.ts
@@ -4,6 +4,8 @@ import { commandMapForward, commandMapBack } from "./commands/command_map.js";
 import { commandExplore } from "./commands/command_explore.js";
 import { commandCatch } from "./commands/command_catch.js";
 import { commandInspect } from "./commands/command_inspect.js";
+import { commandPokedex } from "./commands/command_pokedex.js";
+
 
 import type { CLICommand } from "./state.js";
 
@@ -43,6 +45,11 @@ export function getCommands(): Record<string, CLICommand> {
             name: "inspect <pokemon_name>",
             description: "View details about a caught pokemon",
             callback: commandInspect,
+        },
+        pokedex: {
+            name: "pokedex",
+            description: "See all the pokemon you've caught",
+            callback: commandPokedex,
         },
     };
 }
