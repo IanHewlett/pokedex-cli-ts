b42dd284b8a3e28513b789f6a15a5c0bdab34019
IanHewlett <ian.w.hewlett@gmail.com>
2026-02-12 23:12:18 -0800

    add pokeAPI

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,6 @@
 import { State } from "./state.js";
 
-export function startREPL(state: State) {
+export async function startREPL(state: State) {
     state.readline.prompt();
 
     state.readline.on("line", async (input) => {
@@ -11,6 +11,7 @@ export function startREPL(state: State) {
         }
 
         const commandName = words[0];
+
         const cmd = state.commands[commandName];
         if (!cmd) {
             console.log(
@@ -21,9 +22,9 @@ export function startREPL(state: State) {
         }
 
         try {
-            cmd.callback(state);
+            await cmd.callback(state);
         } catch (e) {
-            console.log(e);
+            console.log((e as Error).message);
         }
 
         state.readline.prompt();
